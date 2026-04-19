efed05e59d8823e3896a842f4361df35444564ea
Zoltán Fülöp <zoltan.fulop21@gmail.com>
2026-04-19 18:37:08 +0200

    Tizenharmadik mentés - pénznem bővítés

--- routes/web.php
+++ routes/web.php
@@ -286,6 +286,39 @@ function normalizeCurrencyCode(?string $currency): string
     return preg_replace('/\s+/', '', $normalized);
 }
 
+function defaultCurrencyCodes(): array
+{
+    return [
+        'AED','AFN','ALL','AMD','ANG','AOA','ARS','AUD',
+        'AWG','AZN','BAM','BBD','BDT','BGN','BHD','BIF',
+        'BMD','BND','BOB','BRL','BSD','BTN','BWP','BYN',
+        'BZD','CAD','CDF','CHF','CLP','CNY','COP','CRC',
+        'CUC','CUP','CVE','CZK','DJF','DKK','DOP','DZD',
+        'EGP','ERN','ETB','EUR','FJD','FKP','GBP','GEL',
+        'GGP','GHS','GIP','GMD','GNF','GTQ','GYD','HKD',
+        'HNL','HRK','HTG','HUF','IDR','ILS','IMP','INR',
+        'IQD','IRR','ISK','JEP','JMD','JOD','JPY','KES',
+        'KGS','KHR','KMF','KPW','KRW','KWD','KYD','KZT',
+        'LAK','LBP','LKR','LRD','LSL','LYD','MAD','MDL',
+        'MGA','MKD','MMK','MNT','MOP','MRU','MUR','MVR',
+        'MWK','MXN','MYR','MZN','NAD','NGN','NIO','NOK',
+        'NPR','NZD','OMR','PAB','PEN','PGK','PHP','PKR',
+        'PLN','PYG','QAR','RON','RSD','RUB','RWF','SAR',
+        'SBD','SCR','SDG','SEK','SGD','SHP','SLL','SOS',
+        'SRD','SSP','STN','SVC','SYP','SZL','THB','TJS',
+        'TMT','TND','TOP','TRY','TTD','TWD','TZS','UAH',
+        'UGX','USD','UYU','UZS','VES','VND','VUV','WST',
+        'XAF','XCD','XOF','XPF','YER','ZAR','ZMW','ZWL',
+    ];
+}
+
+function ensureDefaultCurrenciesExist(): void
+{
+    foreach (defaultCurrencyCodes() as $code) {
+        penznem::firstOrCreate(['nev' => $code], ['nev' => $code]);
+    }
+}
+
 function findOrCreateCurrency(string $currencyInput): ?penznem
 {
     $currencyCode = normalizeCurrencyCode($currencyInput);
@@ -362,6 +395,7 @@ Route::get('/fooldal', function () {
     $userId = $currentUser->id;
 
     $exchangeRateService = new ExchangeRateService();
+    ensureDefaultCurrenciesExist();
 
     $tranzakciok = Tranzakcio::with('kategoria', 'penznem')
         ->where('felhasznaloid', $userId)
